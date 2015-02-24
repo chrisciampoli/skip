20f132156f5c8bd0da88e273231b782bfdd7bb51
Chris Ciampoli <chris@meetsoci.com>
2015-02-23 20:25:45 -0800

    Update to server tests

--- test/test.js
+++ test/test.js
@@ -3,12 +3,12 @@ var app = require('../app.js');
  
 describe('GET /', function() {
   it('respond with hello jenkins', function(done) {
-    request(app).get('/api').expect('hello jenkins', done);
+    request(app).get('/api').expect('{"message":"hello jenkins"}', done);
   });
 });
 
 describe('GET /search', function() {
   it('respond with reached search', function(done) {
-    request(app).get('/api/search').expect('reached search', done);
+    request(app).get('/api/search').expect('{"message":"reached search"}', done);
   });
 });
